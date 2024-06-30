6fb6f7d9c070a6098efb13f1c51eb469f08603e2
maikerudev <michenbox@protonmail.com>
2024-07-01 00:28:31 +0100

    Add basic functions to test for insertion

--- src/assets/main.js
+++ src/assets/main.js
@@ -10,6 +10,48 @@ class Vector {
     this.y = y;
   }
 }
+class Chunk extends Vector {
+  constructor(x, y) {
+    super(x, y);
+    this.binInt = 0;
+    this.magnitude = Math.sqrt(x * x + y * y);
+  }
+}
+
+class ChunkManager {
+  constructor() {
+    this.size = 0;
+    this.chunks = [];
+  }
+  insertChunk(chunk) {
+    if (size == 0) {
+      chunks += chunk;
+      return true;
+    }
+
+    let found = 0;
+    let searchSize = size;
+    let searchArray = chunks;
+    while (found == 0) {
+      let i;
+      if (searchSize % 2 != 0) {
+        i = (searchSize - 1) / 2;
+      } else {
+        i = searchSize / 2;
+      }
+
+      if (chunks[i].magnitude == chunk.magnitude) {
+      } else if (chunks[i].magnitude < chunk.magnitude) {
+      } else {
+      }
+    }
+  }
+  addChunk(x, y, value) {
+    let chunk = new Chunk(x, y, value);
+    this.insertChunk(chunk);
+    return chunk;
+  }
+}
 
 window.visualViewport.addEventListener("resize", () => {
   clearTimeout(timerId); // clear the last started timer to prevent multiple function calls.
